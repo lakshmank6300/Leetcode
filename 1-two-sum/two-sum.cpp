c40976b8caf5d57b3f0abec3de647fe3dc86652f
KAMPARAPU LAKSHMAN <119840360+lakshmank6300@users.noreply.github.com>
2025-02-24 02:03:42 +0530

    Time: 11 ms (46.19%) | Memory: 16.9 MB (7.16%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int i=0,j=nums.size()-1;
-        map<int,int>mp;
-        for(int k=0;k<nums.size();k++){
-            if(mp[nums[k]]==0)
-            mp[nums[k]]=k+1;
-            else{
-                if(nums[k]*2==target)
-                    return {mp[nums[k]]-1,k};
-            }
+        map<int,pair<int,int>>mpp;
+        for(int i=0;i<nums.size();i++){
+            mpp[nums[i]].first=i;
+            mpp[nums[i]].second++;
         }
-        for(int m=0;m<nums.size();m++)
-            cout<<mp[nums[m]]<<" ";
-        sort(nums.begin(),nums.end());
-        while(i<j){
-            if(nums[i]+nums[j]==target){
-                return {mp[nums[i]]-1,mp[nums[j]]-1};
-            }
-            if(nums[i]+nums[j]>target){
-                j--;
+        for(int i=0;i<nums.size();i++){
+            if(target-nums[i]==nums[i]){
+                if(mpp[nums[i]].second>1)
+                    return {i,(mpp[nums[i]].first)};
             }
             else{
-                i++;
+            if(mpp[target-nums[i]].second>0)
+                return {i,mpp[target-nums[i]].first};
             }
+            
         }
-        return {i,78};
+        return {-1};
     }
 };
