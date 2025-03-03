11cff692127e7ba92e9cde49b30b1a6ca3e134a9
KAMPARAPU LAKSHMAN <119840360+lakshmank6300@users.noreply.github.com>
2025-03-03 23:46:27 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (69.02%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
-    int solve(vector<vector<int>>&dp,vector<int>&nums,int ind,int prev_ind,int n){
-        if(ind==n)
-            return 0;
-        if(dp[ind][prev_ind+1]!=-1)
-            return dp[ind][prev_ind+1];
-        int take=0+solve(dp,nums,ind+1,prev_ind,n);
-        if(prev_ind==-1 || ind-prev_ind>1){
-            take=max(take,nums[ind]+solve(dp,nums,ind+1,ind,n));
-        }
-        return dp[ind][prev_ind+1]=take;
-
-    }
     int rob(vector<int>& nums) {
-        vector<vector<int>>dp(nums.size(),vector<int>(nums.size()+2,-1));
-        
-        int res=solve(dp,nums,0,-1,nums.size());
-        // for(int i=0;i<dp.size();i++){
-        //     for(int j=0;j<dp[i].size();j++){
-        //         cout<<dp[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-        return res;
+        int n=nums.size();
+        vector<int>dp(n+1,0);
+        if(n==1)    return nums[0];
+        else if(n==2)   return max(nums[0],nums[1]);
+        dp[0]=nums[0];
+        dp[1]=nums[1];
+        dp[2]=nums[0]+nums[2];
+        for(int i=3;i<n;i++){
+            dp[i]+=max(dp[i-2],dp[i-3])+nums[i];
+        }
+        return max(dp[n-1],dp[n-2]);
     }
 };
