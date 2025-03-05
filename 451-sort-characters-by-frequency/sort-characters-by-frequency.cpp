70b3da76915de37e562badd19939a397264ca373
KAMPARAPU LAKSHMAN <119840360+lakshmank6300@users.noreply.github.com>
2025-03-05 22:00:40 +0530

    Time: 4 ms (37.19%) | Memory: 11.1 MB (59.37%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,28 +1,18 @@
+bool compare(pair<char,int>a,pair<char,int>b){
+    return a.second>b.second;
+}
 class Solution {
 public:
     string frequencySort(string s) {
-        map<char,int>cnt;
-        vector<int>count;
-        for(int i=0;i<s.size();i++){
-            cnt[s[i]]++;
-        }
-        map<int,char>mp;
-        int mx=0;
+        map<char,int>mpp;
         string res="";
-        for(auto i=cnt.begin();i!=cnt.end();i++){
-            if(mx<i->second)
-                mx=i->second;
-        }
-        vector<vector<char>>ans(mx+1);
-        for(int i=0;i<s.size();i++){
-            ans[cnt[s[i]]].push_back(s[i]);
-        }
-        for(int i=0;i<ans.size();i++){
-            sort(ans[i].begin(),ans[i].end());
-        }
-        for(int i=ans.size()-1;i>=0;i--){
-            for(int j=0;j<ans[i].size();j++){
-                res+=ans[i][j];
+        for(int i=0;i<s.size();i++) mpp[s[i]]++;
+        vector<pair<char,int>>vec(mpp.begin(),mpp.end());
+        sort(vec.begin(),vec.end(),compare);
+        for(int i=0;i<vec.size();i++){
+            int sec=vec[i].second;
+            while((sec)--){
+                res+=vec[i].first;
             }
         }
         return res;
