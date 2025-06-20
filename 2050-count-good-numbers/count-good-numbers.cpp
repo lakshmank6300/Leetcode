32b905319586155d58f730e131502eede3740648
KAMPARAPU LAKSHMAN <119840360+lakshmank6300@users.noreply.github.com>
2025-06-20 11:43:33 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (35.03%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.cpp
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -1,26 +1,21 @@
+#define ll long long
+
 class Solution {
 public:
-    long long mod=1e9+7;
-    map<int,int>mpp;
-    long long power(long long a,long long b){
-        // if(b==1) return a;
-        if(b==0) return mpp[b];
-        long long left = power(a,b/2)%mod;
-        if(b%2==1){
-            left=(left*left)%mod;
-            left=(left*a)%mod;
+    int mod = 1e9+7;
+    ll binExp(ll a, ll b){
+        ll res = 1;
+
+        while(b!=0){
+            if(b % 2 == 1) res = (res * a) % mod ;
+            a = (a * a) % mod;
+            b>>=1;
         }
-        else {
-            left=(left*left)%mod;
-        }
-        return mpp[b]=left;
+        return res;
     }
+
+
     int countGoodNumbers(long long n) {
-        mpp[0]=1;
-        long long five=power(5,(n+1)/2);
-        mpp.clear();
-        mpp[0]=1;
-        long long four=power(4,n/2);
-        return (five*four)%mod;
+        return (binExp(5,n - n/2)*binExp(4,n/2)) % mod;
     }
 };
