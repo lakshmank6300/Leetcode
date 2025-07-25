f836362f5a9e105fe7f850ea023f4e4b8b38d1b4
KAMPARAPU LAKSHMAN <119840360+lakshmank6300@users.noreply.github.com>
2025-07-25 23:39:33 +0530

    Time: 39 ms (5.22%) | Memory: 22.2 MB (9.49%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    bool solve(int i,string &s,map<string,bool>&mpp,int n,vector<int>&dp){
-        if(dp[i]!=-1)
-            return dp[i];
-        bool ans=false;
-        if(i>=n) return true;
-        for(auto [word,tr]: mpp){
-            if(word[0]==s[i]){
-                int m=word.size();
-                if(i+m<=n){
-                    string str=s.substr(i,m);
-                    if(str==word){
-                        ans|=solve(i+m,s,mpp,n,dp);
-                    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.size();
+        vector<int>dp(n+1,0);
+        map<string,bool> mpp;
+        for(int i = 0; i < wordDict.size(); i++){
+            mpp[wordDict[i]] = true;
+        }
+        for(int i = 0;i < n;i++){
+            for(int j = i; j >= 0; j--){
+                string str = s.substr(j,i-j+1);
+                // cout<<str<<" ";
+                if(mpp[str] == true && (j == 0 || dp[j-1]==true)){
+                    dp[i] = true;
                 }
             }
         }
-        return dp[i]=ans;
-    }
-    bool wordBreak(string s, vector<string>& wordDict) {
-        map<string,bool>mpp;
-        vector<int>dp(s.size()+1,-1);
-        for(int i=0;i<wordDict.size();i++) mpp[wordDict[i]]=true;
-        return solve(0,s,mpp,s.size(),dp);
+
+        return dp[n-1];
     }
 };
