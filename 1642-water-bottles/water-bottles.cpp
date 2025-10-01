fbd71137e7c1de782cb3672f8054b94b8273553e
KAMPARAPU LAKSHMAN <119840360+lakshmank6300@users.noreply.github.com>
2025-10-01 12:29:29 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (45.09%) - LeetSync

--- 1642-water-bottles/water-bottles.cpp
+++ 1642-water-bottles/water-bottles.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
-        int res=0,rem=0;
-        if(numExchange > numBottles)
-            return numBottles;
-        while(numBottles>0){
-            res+=numBottles;
-            rem+=numBottles%numExchange;
-            if(rem>=numExchange){
-                rem%=numExchange;
-                rem++;
-                res++;
-            }
-            // cout<<res<<" ";
-            numBottles/=numExchange;
+        int res = 0,prev = 0;
+        while(numBottles > 0){
+            res += numBottles;
+            int rem = (numBottles + prev);
+            prev = rem % numExchange;
+            numBottles = (rem / numExchange);
         }
-        cout<<rem;
-        if(rem>=numExchange)
-            res++;
-    return res;
+        return res;
     }
 };
